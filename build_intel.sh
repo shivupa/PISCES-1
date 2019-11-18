7ef2a27b2b8646c1d9b5f35e6d93f4341867ee85
Shiv Upadhyay <shivnupadhyay@gmail.com>
2019-11-18 13:32:01 -0500

    Update build script for cluster (#12)

--- build_intel.sh
+++ build_intel.sh
@@ -7,12 +7,14 @@ module load intel/2017.3.196 \
  intel-mpi/2017.3.196 \
  fftw/3.3.7 \
  arpack/1 \
- cmake/3.13.3
+ cmake/3.13.3 \
+ doxygen/1.8.13
 
 
 CC=mpiicc \
 CXX=mpiicpc \
 FC=mpiifort \
 CXXFLAGS="-Wall -O3" \
-cmake .. -DHAVE_ARPACK=1 -DHAVE_FFTW=1 -DFFTW_OMP_LIBRARY=$FFTWROOT
+cmake .. -DHAVE_ARPACK=1 -DHAVE_FFTW=1 -DFFTW_OMP_LIBRARY=$FFTWROOT -DPISCES_DOCS=1
 make
+make doc
